1cf29cd5f5663cb5bb7d6243068a0084844a8cf7
oogy <tylerweldon94@gmail.com>
2020-05-13 14:03:39 -0400

    rework ssh keys

--- vultr-server-app/main.tf
+++ vultr-server-app/main.tf
@@ -1,3 +1,8 @@
+resource "vultr_ssh_key" "provisioner" {
+  name = "provisioner"
+  ssh_key = var.ssh_key
+}
+
 data "vultr_region" "region" {
   filter {
     name   = "name"
@@ -26,5 +31,5 @@ resource "vultr_server" "server" {
   app_id      = data.vultr_application.application.id
   hostname    = var.project_component != null ? "${var.project_name}-${var.project_component}-${count.index}" : "${var.project_name}-${count.index}"
   label       = var.project_component != null ? "${var.project_name}-${var.project_component}-${count.index}" : "${var.project_name}-${count.index}"
-  ssh_key_ids = var.module_ssh_keys
+  ssh_key_ids = [vultr_ssh_key.provisioner.id]
 }
