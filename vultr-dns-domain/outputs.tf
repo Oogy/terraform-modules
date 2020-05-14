49598b8dc0d6bd187c79d17a88b889ee923e5e7f
oogy <tylerweldon94@gmail.com>
2020-05-14 10:07:36 -0400

    fix undeclard resource and specify instance key

--- vultr-dns-domain/outputs.tf
+++ vultr-dns-domain/outputs.tf
@@ -1,3 +1,3 @@
 output "project_domain" {
-  value = var.domain_exists ? data.vultr_dns_domain[0].domain : vultr_dns_domain.project_domain[0].domain
+  value = var.domain_exists ? data.vultr_dns_domain.project_domain[0].domain : vultr_dns_domain.project_domain[0].domain
 }
